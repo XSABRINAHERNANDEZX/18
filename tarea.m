4f22a24062d25d2c7c21d4a87c6d0e71be36d494
SABRINA HERNANDEZ <94427366+XSABRINAHERNANDEZX@users.noreply.github.com>
2021-11-24 18:29:19 -0600

    CALCULO DIREFENCIAL
    .

---
+++ tarea.m
@@ -0,0 +1,169 @@
+% Título : POLINOMIALES Y RACIONALES
+% Autor :SABRINA ANDREA HERNANDEZ
+% Descripción:GRAFICAR UNA FUNCION Y DETERMINAR SI ES POLINOMICA O RACIONAL 
+% Fecha :20211119
+% Versión :1
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+1 
+clear
+
+% Funcion a plotear
+ir=(3/7)^r
+
+% Función plotear
+plot(ir)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+2
+clear
+
+% Funcion a plotear
+fx= 3^x
+
+% Función plotear
+plot (fx);
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+3
+clear
+
+% Funcion a plotear
+fx= exp(x)
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+4
+clear
+
+% Funcion a plotear
+sx=log10(8*x)
+
+% Función plotear
+plot(sx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+5
+clear
+
+% Funcion a plotear
+fx= log(3*x)
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+6
+clear
+
+% Funcion a plotear
+fx= log(e.^x)
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+7
+clear
+
+% Funcion a plotear
+cv=pecado(v)
+
+% Función plotear
+plot (cv)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+8
+clear
+
+% Funcion a plotear
+fx=cos(x)
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+9
+clear
+
+% Funcion a plotear
+fx=bronceado(x)
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+10
+clear
+
+% Funcion a plotear
+hx=csc(x)
+
+% Función plotear
+plot (hx);
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+11
+clear
+
+% Funcion a plotear
+fx=seg(x)
+
+% Función plotear
+plot ( fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+12
+clear
+
+% Funcion a plotear
+fx=cuna(x)
+
+% Función plotear
+plot (fx)
+
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+13
+clear
+
+% Funcion a plotear
+rx=(7/9).^2*x+3
+
+% Plotear funcion1
+plot(rx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+14
+clear
+
+% Funcion a plotear
+fx= (9^x-2)
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+15
+clear
+
+% Funcion a plotear
+fx=exp(9*x +18);
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+16
+clear
+
+% Funcion a plotear
+vx=(pecado(6*x)-(10))
+
+% Función plotear
+plot(vx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+17
+clear
+
+% Funcion a plotear
+x=[-3*pi:0.1:3*pi]
+y=cos(x.^2+3)
+
+% Plot función
+parcela(x,y,"m" )
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+18
+clear
+
+% Funcion a plotear
+fx=log(cos(x))
+
+% Función plotear
+plot (fx)
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
